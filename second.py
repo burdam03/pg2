6d59570438aec80e9374cec4d5962723eecb197a
burdam03 <burdam03@jcu.cz>
2024-10-15 16:57:30 +0000

    cislo_text

--- second.py
+++ second.py
@@ -1,7 +1,25 @@
 def cislo_text(cislo):
-    # funkce zkonvertuje cislo do jeho textove reprezentace
-    # napr: "25" -> "dvacet pět", omezte se na cisla od 0 do 100
-    return "dvacet pět"
+    if int(cislo)<=100:
+        desitky = ["","","dvacet","třicet","čtyřicet","padesát","šedesát", "sedmdesát","osmdesát","devadesát"]
+        jednotky = ["", "jedna", "dva", "tři", "čtyři", "pět", "šest", "sedm", "osm", "děvět"]
+
+        if len(cislo)==3:
+            if (cislo[0]=="1" and cislo[1]=="0" and cislo[2]=="0"):
+                cislo_cislo = "sto"
+
+        if len(cislo)==2:
+            if cislo[0] == "1":
+                teens = ["deset", "jedenáct","dvanáct","třináct","čtrnáct", "patnáct", "šestnáct","sedmnáct", "osmnáct","devatenáct"]
+                cislo_cislo = teens[int(cislo[1])]
+            else:
+                cislo_cislo = desitky[int(cislo[0])]+ " " + jednotky[int(cislo[1])]
+        if len(cislo)==1:
+            jednotky[0] = "nula"
+            cislo_cislo = jednotky[int(cislo[0])]
+    else:
+        cislo_cislo = "Větší než sto"
+    return cislo_cislo
+
 
 if __name__ == "__main__":
     cislo = input("Zadej číslo: ")
